f5b2444e0496e85dbc216684d1b9668c1e7a5741
Dan <dan.tran301@gmail.com>
2021-09-13 12:09:39 -0700

    First attempt

--- 709_To_Lower_Case.java
+++ 709_To_Lower_Case.java
@@ -1,14 +1,23 @@
 class Solution {
-    public String toLowerCase(String str) {
-        char[] a = str.toCharArray();
+    public String toLowerCase(String s) {
+        // convert string to array
+        char[] letters = s.toCharArray();
 
-        for (int i = 0; i < a.length; i++) {
-            if ('A' <= a[i] && a[i] <= 'Z') {
-                a[i] = (char) (a[i] - 'A' + 'a');
+        for (int i = 0; i < letters.length; i++) {
+            // if the letter we are on is between or equal to A and Z
+            // then it is a capital letter
+            // ASCII table 'A' is 65 and 'Z' is 90
+            if ('A' <= letters[i] && letters[i] <= 'Z') {
+                // subtract 'A' is current letter - 65 to get it to null or if you're on 'B' its gets
+                // you to null + 1 so when you add 'a' is gets the lowercase version of the letter you're on
+                // convert from int to char
+                letters[i] = (char) (letters[i] - 'A' + 'a');
 
             }
+
         }
 
-        return new String(a);
+        // return the array as string form
+        return new String(letters);
     }
 }
