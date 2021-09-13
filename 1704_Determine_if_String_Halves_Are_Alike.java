79cb0cd5f0110a0693cbcac804cf66fcc61125e9
Dan <dan.tran301@gmail.com>
2021-09-13 11:45:40 -0700

    First attempt

--- 1704_Determine_if_String_Halves_Are_Alike.java
+++ 1704_Determine_if_String_Halves_Are_Alike.java
@@ -1,9 +1,18 @@
-public boolean halvesAreAlike(String s) {
+class Solution {
+    public boolean halvesAreAlike(String s) {
         var vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
-        int a = 0, b = 0;
-        for (int i = 0, j = s.length() - 1; i < j; ++i, --j) {
-        a += vowels.contains(s.charAt(i)) ? 1 : 0;
-        b += vowels.contains(s.charAt(j)) ? 1 : 0;
+        int count1 = 0, count2 = 0;
+
+        // setup our 2 points to loop through string
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            // count1 tracks first half
+            count1 += vowels.contains(s.charAt(i)) ? 1 : 0;
+            // count2 tracks second half
+            count2 += vowels.contains(s.charAt(j)) ? 1 : 0;
         }
-        return a == b;
-        }
+
+        return count1 == count2;
+    }
+}
+
+ 
