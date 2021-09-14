905900248431a2d0e51f6cd0383cf2d88e2a5058
Dan <dan.tran301@gmail.com>
2021-09-14 12:50:07 -0700

    First attempt

--- 1221_Split_a_String_in_Balanced_Strings.java
+++ 1221_Split_a_String_in_Balanced_Strings.java
@@ -1,15 +1,23 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        int result = 0;
-        int sum = 0;
+        char[] letters = s.toCharArray();
 
-        for (char letter : s.toCharArray()) {
-            sum += (letter == 'R' ? 1 : -1);
-            if (sum == 0) {
-                result++;
+        // gets the sum of a balanced right and left
+        int sumOfRL = 0;
+        // counts the number of balanced strings
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // if letters equals 'R' then add 1 otherwise subtract 1
+            // this works because string is always balanced
+            sumOfRL += letters[i] == 'R' ? 1 : -1;
+
+            // if our sum is 0 then it means we have balance so we count it
+            if (sumOfRL == 0) {
+                count+=1;
             }
         }
 
-        return result;
+        return count;
     }
 }
