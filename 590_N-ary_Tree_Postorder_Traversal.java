05210394cb32f86cb3baac03c3e8d50da921eaea
Dan <dan.tran301@gmail.com>
2021-09-15 13:30:14 -0700

    First attempt

--- 590_N-ary_Tree_Postorder_Traversal.java
+++ 590_N-ary_Tree_Postorder_Traversal.java
@@ -19,3 +19,30 @@ class Solution {
     }
 }
 
+// ITERATIVE
+class Solution {
+    public List<Integer> postorder(Node root) {
+        // stack stores our tree node references
+        LinkedList<Node> stack = new LinkedList<>();
+        // stores our values from each node
+        LinkedList<Integer> output_arr = new LinkedList<>();
+        // add our root into the stack
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            // grab the last value in our stack (which is also the top of the stack)
+            Node node = stack.pollLast();
+            // add the value to our output
+            output_arr.addFirst(node.val);
+
+            // repeat for each child node in the tree
+            // its 1 first
+            // then it would be 3, 2, 4 and 4 is the top of the stack
+            for (Node child : node.children) {
+                stack.add(child);
+            }
+        }
+
+        return output_arr;
+    }
+}
