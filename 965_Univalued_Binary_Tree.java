cdde335ce50b9f078211a770f27d70c098ad2715
Dan <dan.tran301@gmail.com>
2021-09-20 18:39:36 -0700

    First attempt

---
+++ 965_Univalued_Binary_Tree.java
@@ -0,0 +1,8 @@
+class Solution {
+    int val = -1;
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) return true;
+        if (val < 0) val = root.val;
+        return root.val == val && isUnivalTree(root.left)  && isUnivalTree(root.right);
+    }
+}
